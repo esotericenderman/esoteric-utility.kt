086e552773c7f8228b92ccf79615c3649ba4704d
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-28 21:26:24 +0000

    Organise test resources

--- lib/src/test/kotlin/foundation/esoteric/utility/resource/ResourceUtilityTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/utility/resource/ResourceUtilityTest.kt
@@ -8,6 +8,6 @@ class ResourceUtilityTest {
     @Test fun resourcesListIsCorrect() {
         val resourcePaths = ResourceUtility.getResourceFilePaths("resource")
 
-        assertEquals(resourcePaths, setOf(Path.of("resource/Test Folder/Test File.txt"), Path.of("resource/Test File.txt")))
+        assertEquals(resourcePaths, setOf(Path.of("resource/ResourceUtilityTest/Test Folder/Test File.txt"), Path.of("resource/ResourceUtilityTest/Test File.txt")))
     }
 }
