5577f44be522fd67774e105ba205683cbd3b1d22
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-28 20:59:34 +0000

    Add equality assertion

--- lib/src/test/kotlin/foundation/esoteric/utility/resource/ResourceUtilityTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/utility/resource/ResourceUtilityTest.kt
@@ -1,11 +1,13 @@
 package foundation.esoteric.utility.resource
 
+import java.nio.file.Path
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ResourceUtilityTest {
     @Test fun resourcesListIsCorrect() {
         val resourcePaths = ResourceUtility.getResourceFilePaths("resource")
 
-        println(resourcePaths.toString())
+        assertEquals(resourcePaths, setOf(Path.of("resource/Test Folder/Test File.txt"), Path.of("resource/Test File.txt")))
     }
 }
