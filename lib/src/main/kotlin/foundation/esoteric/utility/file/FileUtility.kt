12028563c281bcb8b75324ef90efbf9b67695497
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-28 20:48:01 +0000

    Allow file utility method to use `Path`s also

--- lib/src/main/kotlin/foundation/esoteric/utility/file/FileUtility.kt
+++ lib/src/main/kotlin/foundation/esoteric/utility/file/FileUtility.kt
@@ -1,6 +1,7 @@
 package foundation.esoteric.utility.file
 
 import java.io.File
+import java.nio.file.Path
 
 /**
  * This class provides various static utility methods for working with files.
@@ -33,6 +34,16 @@ class FileUtility {
             }
 
             return true
+        }
+
+        /**
+         * This method checks if a directory is **recursively empty**, which means either:
+         * - The directory is empty.
+         * - The directory contains only **recursively empty** directories.
+         * @param directoryPath The path of the directory to check for recursive emptiness.
+         */
+        fun isRecursivelyEmpty(directoryPath: Path): Boolean {
+            return isRecursivelyEmpty(directoryPath.toFile())
         }
 
         /**
