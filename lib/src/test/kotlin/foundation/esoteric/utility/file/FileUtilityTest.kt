a7a925f91d6db661ce7d711dea87fc47ac37327f
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-29 18:27:03 +0000

    Check for thrown exception when file is not directory

--- lib/src/test/kotlin/foundation/esoteric/utility/file/FileUtilityTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/utility/file/FileUtilityTest.kt
@@ -2,6 +2,7 @@ package foundation.esoteric.utility.file
 
 import org.apache.commons.io.FileUtils
 import java.io.File
+import kotlin.math.exp
 import kotlin.test.*
 
 class FileUtilityTest {
@@ -39,6 +40,22 @@ class FileUtilityTest {
         File("run/directory/file.txt").createNewFile()
 
         assertFalse(FileUtility.isRecursivelyEmpty(run))
+    }
+
+    @Test fun recursiveEmptinessThrowsWhenNotDirectory() {
+        val file = File("run/file.txt")
+
+        file.createNewFile()
+
+        var expected: Exception? = null
+
+        try {
+            FileUtility.isRecursivelyEmpty(file)
+        } catch (exception: Exception) {
+            expected = exception
+        }
+
+        assertNotNull(expected)
     }
 
     @AfterTest fun deleteRunDirectory() {
