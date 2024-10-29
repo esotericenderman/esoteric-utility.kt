e044c4ddc93b68ff7fc74491df888e51abc1b23b
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-29 18:24:14 +0000

    Test the thrown exception

--- lib/src/test/kotlin/foundation/esoteric/utility/resource/ResourceUtilityTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/utility/resource/ResourceUtilityTest.kt
@@ -3,11 +3,24 @@ package foundation.esoteric.utility.resource
 import java.nio.file.Path
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class ResourceUtilityTest {
     @Test fun resourcesListIsCorrect() {
         val resourcePaths = ResourceUtility.getResourceFilePaths("resource")
 
         assertEquals(resourcePaths, setOf(Path.of("resource/ResourceUtilityTest/Test Folder/Test File.txt"), Path.of("resource/ResourceUtilityTest/Test File.txt")))
+    }
+
+    @Test fun getResourcePathsThrowsCorrectly() {
+        var expected: Exception? = null
+
+        try {
+            ResourceUtility.getResourceFilePaths("some random path that does not exist")
+        } catch (exception: Exception) {
+            expected = exception
+        }
+
+        assertNotNull(expected)
     }
 }
