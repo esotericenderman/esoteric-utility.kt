f70a47b77e99ca243ac7e5d16456d1e7fe6624ad
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-29 21:55:39 +0000

    Add documentation to string utility

--- lib/src/main/kotlin/foundation/esoteric/utility/string/StringUtility.kt
+++ lib/src/main/kotlin/foundation/esoteric/utility/string/StringUtility.kt
@@ -4,6 +4,10 @@ import java.security.MessageDigest
 
 class StringUtility {
     companion object {
+        /**
+         * This method returns the SHA-1 hash of the input string.
+         * @param input The string to find the SHA-1 hash of.
+         */
         fun getSha1Hash(input: String): String {
             val bytes = MessageDigest.getInstance("SHA-1").digest(input.toByteArray())
             return bytes.joinToString("") { "%02x".format(it) }
